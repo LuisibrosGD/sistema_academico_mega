74c8beb7504220b6d96133026550e0e626586d36
LuisibrosGD <luisortysgf@gmail.com>
2025-07-05 19:23:05 -0500

    mejora de la base de datos en la conexion. Ahora puedes usar store procediums

--- sistema_mega/database/conexion.py
+++ sistema_mega/database/conexion.py
@@ -37,6 +37,45 @@ def ejecutar_modificacion(query, datos=None):
             print("✅ Consulta ejecutada correctamente")
         except mysql.connector.Error as err:
             print(f"❌ Error en la modificación: {err}")
+            conexion.rollback()  # 🔥 Muy importante
         finally:
             cursor.close()
             conexion.close()
+
+# Ejecutar un procedimiento almacenado sin OUT. Con solo IN
+def ejecutar_procedimiento(nombre_proc, parametros=None):
+    conexion = obtener_conexion()
+    if conexion:
+        cursor = conexion.cursor()
+        try:
+            cursor.callproc(nombre_proc, parametros or [])
+            conexion.commit()
+            print("✅ Procedimiento ejecutado correctamente")
+        except mysql.connector.Error as err:
+            print(f"❌ Error al ejecutar el procedimiento: {err}")
+            conexion.rollback()
+        finally:
+            cursor.close()
+            conexion.close()
+
+# Ejecutar procedimiento con OUT
+def ejecutar_procedimiento_con_out(nombre_proc, parametros=None):
+    conexion = obtener_conexion()
+    if conexion:
+        cursor = conexion.cursor()
+        try:
+            # Si hay parámetros, pásalos; si no, llama sin ellos
+            if parametros is not None:
+                resultados = cursor.callproc(nombre_proc, parametros)
+            else:
+                resultados = cursor.callproc(nombre_proc)
+
+            conexion.commit()
+            return resultados  # Lista con IN y OUT
+        except mysql.connector.Error as err:
+            print(f"❌ Error al ejecutar el procedimiento con OUT: {err}")
+            conexion.rollback()
+        finally:
+            cursor.close()
+            conexion.close()
+
